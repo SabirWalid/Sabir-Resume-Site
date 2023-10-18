581043ae1f867052f36141612fa470aa84c8fc6a
SabirWalid <s.abdurahma@alustudent.com>
2023-10-18 10:36:13 +0200

    My resume

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// JavaScript code for your website
-
 // Function to change the background color of the header when the user clicks on it
 const header = document.querySelector('header');
 header.addEventListener('click', function() {
@@ -10,7 +8,7 @@ header.addEventListener('click', function() {
     
 });
 
-// Function to toggle a dark mode for your website
+// Function to toggle a dark mode for the website
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const body = document.body;
 
@@ -18,19 +16,16 @@ darkModeToggle.addEventListener('click', function() {
     body.classList.toggle('dark-mode');
 });
 
-// Get a reference to the image element
-const clickableImage = document.getElementById('clickable-image');
-
-// Define a function to apply an effect when the image is clicked
-function applyImageEffect() {
-    // Add your desired effect here; for example, change the opacity
-    clickableImage.style.opacity = '0.5';
-    clickableImage.style.filter = 'grayscale(100%)';
-    clickableImage.style.transition = 'opacity 0.5s';
+// script function that prompts the user with  "Ok" or "Cancel" confirmation to view my website,
+function confirmViewWebsite() {
+    const userConfirmed = window.confirm("You are most welcome to Sabir's website, would you like to view it?");
+    if (userConfirmed) {
+        window.location.href = "https://sabirwalid.github.io/Sabir-Resume-Site/";
+    } else {
+        alert("Thank you for considering! Feel free to visit us later.");
+    }
 }
 
-// Add a click event listener to trigger the effect
-clickableImage.addEventListener('click', applyImageEffect);
-clickableImage.addEventListener('mouseover', applyImageEffect);
-clickableImage.addEventListener('mouseout', applyImageEffect);
+const viewButton = document.getElementById("viewButton");
+viewButton.addEventListener("click", confirmViewWebsite);
 
